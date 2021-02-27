4d610eefec1b1b2cb930a19d0556b8ca8c8b9a99
Richard Gustavsson <retg94@gmail.com>
2021-02-27 20:45:09 +0100

    project ready for handin

---
+++ README.txt
@@ -0,0 +1,4 @@
+1. Open index.html in your preffered web-browser.
+
+2.1 Enter the desired city you want some information about.
+2.2 Use the checkboxes if you only want information about weather, attractions or if you want to sort the attraction aplhabetically.
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const getWeatherKey = "7ce12bf5d6ccd49ddae5307dd68fbb4f";
 const AttractionID = "3NNXITF340NHOCWXYL3F5PX20FU5QBDYCZOUMP4W153NFLTB";
 const AttractionSecret = "XGAP1LEIZJZLOMM5OXIPBV3AABCXHXRF3MP2F5FVGXVPSQH5";
-const fourSquareVersion = "20210224"
+const fourSquareVersion = "20210226";
+const foursquareUrl = "https://api.foursquare.com/v2/venues/explore?"
 //const getWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${getWeatherKey}`
 const weatherResultContainer = document.querySelector("#weatherResultContainer")
 const attractionResultContainer = document.querySelector("#attractionResultContainer")
@@ -9,27 +10,33 @@ const form = document.querySelector("form")
 
 class WeatherInfo {
     constructor(name, temperature, condition, iconUrl){
-        this._name = name;
-        this._temperature = temperature;
-        this._condition = condition;
-        this._iconUrl = iconUrl;
+        this.Name = name;
+        this.Temperature = temperature;
+        this.Condition = condition;
+        this.IconUrl = iconUrl;
     }
 }
 class AttractionInfo {
     constructor(name, address, iconUrl){
-        this._name = name;
-        this._address = address;
-        this._iconUrl = iconUrl;
+        this.Name = name;
+        this.Address = address;
+        this.IconUrl = iconUrl;
     }
 }
 
+document.getElementById("myInput").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.code === "Enter") {
+        document.getElementById("myBtn").click();
+    }
+});
+
 async function executeSearch(){
     console.log("hej")
-    console.log(form.elements["search"])
+    //console.log(form.elements["search"])
     weatherResultContainer.innerHTML = "";
     attractionResultContainer.innerHTML = "";
-    const search = form.elements["search"].value;
-    console.log(search)
+    const search = document.getElementById("myInput").value;
     const onlyWeather = document.querySelector("#onlyWeather").checked;
     const onlyAttractions = document.querySelector("#onlyAttractions").checked;
     const filterAlpha = document.querySelector("#filter").checked;
@@ -67,33 +74,40 @@ async function executeSearch(){
     }
 
 }
-
 async function getAttractions(search, filterAlpha){
-    let response = await fetch(`https://api.foursquare.com/v2/venues/explore?client_id=${AttractionID}&client_secret=${AttractionSecret}&near=${search}&v=${fourSquareVersion}&limit=10`);
+    let response = await fetch(`${foursquareUrl}client_id=${AttractionID}&client_secret=${AttractionSecret}&near=${search}&v=${fourSquareVersion}&limit=10`);
     response = await response.json();
     console.log(response);
     let attractionList = [];
     response.response.groups[0].items.forEach(item => {
-        console.log(item)
+        //console.log(item)
         //console.log(item.venue.name + " " + item.venue.location.address)
         attractionList.push(new AttractionInfo(item.venue.name, item.venue.location.address, `${item.venue.categories[0].icon.prefix}64${item.venue.categories[0].icon.suffix}` ))
     })
+    //attractionList.forEach(item => console.log(item.Name))
     //console.log(filterAlpha);
     if(filterAlpha === true){
-        console.log("TRYING TO SORT LIST")
-        attractionList = attractionList.sort(function(a, b){
-            var nameA=a._name.toLowerCase(), nameB=b._name.toLowerCase();
+        //console.log("TRYING TO SORT LIST")
+        attractionList = attractionList.sort( (a, b) => {
+            attractionList.forEach(element => {
+                console.log(element.Name)
+            });
+            console.log("---------------------")  
+            var nameA=a.Name.toLowerCase(), nameB=b.Name.toLowerCase();
             if (nameA < nameB) //sort string ascending
-             return -1;
+                return -1;
             if (nameA > nameB)
-             return 1;
+                return 1;
             return 0; //default return value (no sorting)
         });
-        console.log("IT SHOULD BE SORTED NOW")
+        //venuesArray = _.sortBy( venuesArray, 'name' );
+        //<script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js%22%3E </script>
+        //https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+        //console.log("IT SHOULD BE SORTED NOW")
     }
     attractionList.forEach(attraction => {
         //console.log(attraction._name + " " + attraction._address)
-        createAttractionElement(attraction._name, attraction._address, attraction._iconUrl)
+        createAttractionElement(attraction.Name, attraction.Address, attraction.IconUrl)
     })
 }
 
@@ -104,6 +118,7 @@ function createAttractionElement(name, address, iconUrl){
     let attractionAddress = document.createElement("p");
     let icon = document.createElement("img");
     icon.src = iconUrl;
+    icon.id = "resultIcons";
     attractionName.innerHTML = name;
     attractionAddress.innerHTML = address;
     attractionContainer.appendChild(attractionName);
@@ -119,13 +134,6 @@ async function getWeather(search){
     console.log(response);
     const weatherInfo = new WeatherInfo(response.name, response.main["temp"], response.weather[0].description, `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
     createWeatherElement(weatherInfo);
-  
-    /*
-    if(weatherInfo._isOkay === true){
-        createWeatherElement(weatherInfo);
-    } else {
-        createErrorElement();
-    }*/
 }
 
 function createErrorElement(){
@@ -142,11 +150,12 @@ function createWeatherElement(weatherInfo){
     let weatherTemp = document.createElement("p");
     let weatherCond = document.createElement("p");
     let icon = document.createElement("img");
-    icon.src = weatherInfo._iconUrl;
+    icon.src = weatherInfo.IconUrl;
+    icon.id = "resultIcons";
 
-    weatherName.innerHTML = weatherInfo._name;
-    weatherTemp.innerHTML = `Temperature: ${weatherInfo._temperature} Celsius`;
-    weatherCond.innerHTML = `Condition: ${weatherInfo._condition}`;
+    weatherName.innerHTML = weatherInfo.Name;
+    weatherTemp.innerHTML = `Temperature: ${weatherInfo.Temperature} Celsius`;
+    weatherCond.innerHTML = `Condition: ${weatherInfo.Condition}`;
     weatherContainer.appendChild(weatherName)
     weatherContainer.appendChild(icon)
     weatherContainer.appendChild(weatherTemp)
