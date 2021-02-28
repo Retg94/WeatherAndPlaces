ce81f19db819a3a68500e28162425bbf178be37f
Richard Gustavsson <retg94@gmail.com>
2021-02-28 16:03:34 +0100

    fixed last stuff (for now)

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const AttractionID = "3NNXITF340NHOCWXYL3F5PX20FU5QBDYCZOUMP4W153NFLTB";
 const AttractionSecret = "XGAP1LEIZJZLOMM5OXIPBV3AABCXHXRF3MP2F5FVGXVPSQH5";
 const fourSquareVersion = "20210226";
 const foursquareUrl = "https://api.foursquare.com/v2/venues/explore?";
-const openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?"
+const openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const openWeatherIconUrl = "http://openweathermap.org/img/wn/";
 const weatherResultContainer = document.querySelector("#weatherResultContainer")
 const attractionResultContainer = document.querySelector("#attractionResultContainer")
 const form = document.querySelector("form")
@@ -76,17 +77,13 @@ async function executeSearch(){
 async function getAttractions(search, filterAlpha){
     let response = await fetch(`${foursquareUrl}client_id=${AttractionID}&client_secret=${AttractionSecret}&near=${search}&v=${fourSquareVersion}&limit=10`);
     response = await response.json();
-    console.log(response);
     let attractionList = [];
-    response.response.groups[0].items.forEach(item => {
-        attractionList.push(new AttractionInfo(item.venue.name, item.venue.location.address, `${item.venue.categories[0].icon.prefix}64${item.venue.categories[0].icon.suffix}` ))
+    attractionList = response.response.groups[0].items.map(item => {
+        return new AttractionInfo(item.venue.name, item.venue.location.address, `${item.venue.categories[0].icon.prefix}64${item.venue.categories[0].icon.suffix}` )
     })
+    console.log(attractionList)
     if(filterAlpha === true){
         attractionList = attractionList.sort( (a, b) => {
-            attractionList.forEach(element => {
-                console.log(element.Name)
-            });
-            console.log("---------------------")  
             var nameA=a.Name.toLowerCase(), nameB=b.Name.toLowerCase();
             if (nameA < nameB) //sort string ascending
                 return -1;
@@ -118,8 +115,7 @@ function createAttractionElement(name, address, iconUrl){
 async function getWeather(search){
     let response = await fetch(`${openWeatherUrl}q=${search}&units=metric&appid=${getWeatherKey}`);
     response = await response.json();
-    console.log(response);
-    const weatherInfo = new WeatherInfo(response.name, response.main["temp"], response.weather[0].description, `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
+    const weatherInfo = new WeatherInfo(response.name, response.main["temp"], response.weather[0].description, `${openWeatherIconUrl}${response.weather[0].icon}@2x.png`);
     createWeatherElement(weatherInfo);
 }
 
